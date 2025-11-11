45f4d33d2fbf83ed1f7b18119272d623f32fdb4d
StormAvenger <bozinov.david@gmail.com>
2025-11-11 11:10:38 -0800

    Basic Set-up

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-#define GSR_PIN 34          // Use ADC1 pin. Connect GSR SIG here.
-#define NUM_SAMPLES 10      // Simple averaging for less noise
 
+void setup() {
+    // run once
+}
+
+void loop() {
+    // runs forever
+}
